1e7f9e7f653c394d660236abe48cc15a5d116427
Ivan Shadrin <shadrin@smtech.ru>
2023-07-21 17:05:15 +0300

    feat: add cmd lint add eslint to project

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { getNameAndSayHello } from "../src/cli.js";
+import { getNameAndSayHello } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 console.log(getNameAndSayHello());
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export const getNameAndSayHello = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return `Hello, ${userName}!`;
+  const userName = readlineSync.question('May I have your name? ');
+  return `Hello, ${userName}!`;
 };
