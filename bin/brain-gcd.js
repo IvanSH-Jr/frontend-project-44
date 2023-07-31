5fa719ce8451c8ff18d9f69fec669ec786683aac
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-07-31 17:36:29 +0300

    fix: fixing arch is done

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-import startGame from '../src/index.js';
 import gcdGame from '../games/gcd-game.js';
 
-gcdGame(startGame());
+gcdGame();
--- games/gcd-game.js
+++ games/gcd-game.js
@@ -1,29 +1,17 @@
-import { question } from 'readline-sync';
-
 import valueRandomizer from '../src/valueRandomizer.js';
 import euclidGcdSearch from '../src/gcdSearch.js';
+import startGame from '../src/index.js';
 
-const gcdGame = (userName) => {
-  console.log('Find the greatest common divisor of given numbers');
+const gameTask = 'Find the greatest common divisor of given numbers';
+const getGcd = () => {
+  const mathValue1 = valueRandomizer(1, 100);
+  const mathValue2 = valueRandomizer(1, 100);
+  const gcdResult = euclidGcdSearch(mathValue1, mathValue2);
+  const question = `${mathValue1} ${mathValue2}`;
 
-  let rightAnswers = 0;
-  while (rightAnswers < 3) {
-    const mathValue1 = valueRandomizer(1, 100);
-    const mathValue2 = valueRandomizer(1, 100);
-    const gcdResult = euclidGcdSearch(mathValue1, mathValue2);
-
-    console.log(`Question: ${mathValue1} ${mathValue2}`);
-    const userAnswer = question('Your answer: ');
-    if (+userAnswer === gcdResult) {
-      console.log('Correct!');
-      rightAnswers += 1;
-    } else {
-      rightAnswers = 0;
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${gcdResult}'`);
-      break;
-    }
-  }
-  console.log(rightAnswers === 3 ? `Congratulations, ${userName}!` : `Let's try again, ${userName}!`);
+  return [question, gcdResult]
 };
+
+const gcdGame = () => startGame(getGcd, gameTask);
 
 export default gcdGame;
