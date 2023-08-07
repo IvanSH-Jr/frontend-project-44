952d9a8b3b885d0b1aad3b5fa5f1ef653c785c3b
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-08-07 10:56:05 +0300

    fix: eslint fix in getrandomvalue.js

--- my_modules/getRandomValue.js
+++ my_modules/getRandomValue.js
@@ -1,5 +1,3 @@
-const getRandomValue = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+const getRandomValue = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default getRandomValue;
