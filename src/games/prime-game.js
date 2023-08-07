85cc86338647c4dbfa47942dadf93ab08e1d8618
Ivan Shadrin <shadrin@spbec-mining.ru>
2023-08-07 16:24:05 +0300

    fix: prime-game fix prime checking

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -4,10 +4,11 @@ import startGame from '../index.js';
 const gameTask = 'Answer "yes" if given number is prime. Otherwise answer "no"';
 
 const isPrime = (number) => {
-  for (let i = 2; i < number; i += 1) {
+  if (number < 2) return false;
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) return false;
   }
-  return number !== 1;
+  return true;
 };
 
 const getPrimeGameRigthAnswer = () => {
